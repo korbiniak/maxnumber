08c8808064499630c4b796f13658640b542a6f55
Franciszek Malinka <franciszek.malinka@gmail.com>
2025-05-08 16:31:00 +0200

    Create basic project structure

---
+++ client/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/socket.io/': {
+        target: 'http://localhost:3001',
+        ws: true,
+      },
+      '/api': {
+        target: 'http://jsonplaceholder.typicode.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+})
