e4ea3406835d4bddd9ac0c15b82824d40c6db962
Franciszek Malinka <franciszek.malinka@gmail.com>
2025-05-08 17:58:15 +0200

    Basic types

---
+++ shared/src/types.ts
@@ -0,0 +1,17 @@
+export type PlayerId = string;
+
+export type Digit = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+export type Operation = "*" | "/" | "+" | "-";
+
+export type Card = Digit | Operation;
+
+export type Expression = Card[];
+
+export interface GameState {
+  player1exp: Expression;
+  player2exp: Expression;
+  player1Id: PlayerId;
+  player2Id: PlayerId;
+  currentTurn: 1 | 2;
+  availableCards: Card[];
+}
