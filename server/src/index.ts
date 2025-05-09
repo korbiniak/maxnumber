3ad6bd600917bc88b177906ba9c95300bb7b4c2b
Pr0grammerSimon <szymon@krzywda.eu>
2025-05-09 10:12:34 +0200

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from "express";
+<<<<<<< Updated upstream
 import { GameState } from "shared";
 import http from "http";
 import {Server} from "socket.io";
+=======
+import { GameState, SERVER_PORT } from "../../shared";
+>>>>>>> Stashed changes
 
 
 const app = express();
@@ -10,7 +14,14 @@ const io = new Server(server);
 
 
 // It would be nicer if it was defined in shared/constants 
-const port = 3001;
+const port = SERVER_PORT;
+
+
+let current_games = {}; //A dict with current games {id : game}
+let last_game_id = 0; //Last game id (to get a new one)
+
+let players_queue = []; //A queue of current waiting players (id)
+//players_queue.shift();
 
 // Define the root path with a greeting message
 app.get("/", (req: Request, res: Response) => {
@@ -22,6 +33,15 @@ app.listen(port, () => {
     console.log(`The server is running at http://localhost:${port}`);
 });
 
+<<<<<<< Updated upstream
 io.on('connection', (socket) => {
     
 })
+=======
+
+
+
+
+
+
+>>>>>>> Stashed changes
