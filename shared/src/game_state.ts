81cd1abb06672f54131b4c0b4858fd138ee7a022
Pr0grammerSimon <szymon@krzywda.eu>
2025-05-09 10:12:02 +0200

    Create basic backend with a queue of waiting users paired immediately.

---
+++ shared/src/game_state.ts
@@ -0,0 +1,24 @@
+import { ALL_DIGIT_CARDS, ALL_OPERATION_CARDS, NUMBER_DIGIT_CARDS_IN_GAME, NUMBER_OPERATION_CARDS_IN_GAME } from "./constants";
+import { PlayerId, Card, GameState } from "./types";
+
+export const ets = "";
+
+//Create a game for two players and return an instance of game state
+export function initGameState(player_id_1 : PlayerId, player_id_2 : PlayerId) : GameState{
+
+    let cards : Card[] = [];
+    //get a random cards 
+    for (let nr_card = 0; nr_card < NUMBER_DIGIT_CARDS_IN_GAME; nr_card++){
+        cards.push(ALL_DIGIT_CARDS[Math.floor(Math.random() * ALL_DIGIT_CARDS.length)]);
+    }
+
+    for (let nr_card = 0; nr_card < NUMBER_OPERATION_CARDS_IN_GAME; nr_card++){
+        cards.push(ALL_OPERATION_CARDS[Math.floor(Math.random() * ALL_OPERATION_CARDS.length)]);
+    }
+
+
+    //Init a game
+    let game : GameState = {player1Id : player_id_1, player2Id : player_id_2, player1exp : [1], player2exp : [1], currentTurn : (Math.floor(Math.random() * 2) + 1 as 1 | 2), availableCards : cards};
+
+    return game;
+}
--- shared/src/utils.ts
+++ shared/src/utils.ts
@@ -1,2 +1,40 @@
-import  './types'
+import { ALL_OPERATION_CARDS } from './constants';
+import { Expression, Operation} from './types'
 
+
+export function isExpressionValid(expr : Expression){
+
+    if (expr.length == 0) return true;
+    if (expr[0] == "*" || expr[0] == "/") return false;
+    if (expr[expr.length - 1] in ALL_OPERATION_CARDS) return false;
+
+    for (let nr_card = 1; nr_card < expr.length; nr_card++){
+        if ((expr[nr_card] in ALL_OPERATION_CARDS) && (expr[nr_card - 1] in ALL_OPERATION_CARDS)) return false;
+    }
+
+    return true;
+
+}
+
+
+export function isExtensionOfExpression(expr1 : Expression, expr2 : Expression) : boolean{
+
+    if (expr1.length + 1 != expr2.length) return false;
+
+    let diff_occurs = false;
+
+    for (let idx = 0; idx < expr2.length; idx++){
+
+        if (diff_occurs){
+            if (expr1[idx - 1] != expr2[idx]) return false;
+        }
+        else if (idx < expr1.length && expr1[idx] != expr2[idx]){
+            diff_occurs = true;
+        }
+    }
+
+
+    return true;
+
+
+}
