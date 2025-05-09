00672194ef7d403172f6f5d00e2ad8cfa6146873
Pr0grammerSimon <szymon@krzywda.eu>
2025-05-09 22:29:06 +0200

    Some code :D

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import { GameState, SERVER_PORT, PlayerId, Card} from "shared";
+import { GameState, SERVER_PORT, PlayerId, Card, NUMBER_DIGIT_CARDS_IN_GAME, ALL_DIGIT_CARDS, NUMBER_OPERATION_CARDS_IN_GAME, ALL_OPERATION_CARDS} from "shared";
 import http from "http";
 import {Server} from "socket.io";
 
@@ -14,7 +14,7 @@ const port = SERVER_PORT;
 
 
 
-let current_games = {}; //A dict with current games {id : game}
+const current_games = new Map(); //A dict with current games {id : game}
 let last_game_id = 0; //Last game id (to get a new one)
 
 let players_queue : PlayerId[]; //A queue of current waiting players (id)
@@ -31,27 +31,51 @@ app.listen(port, () => {
 }); 
 
 
-function createGame(player_id_1 : PlayerId, player_id_2 : PlayerId | undefined){
+function createGame(player_id_1 : PlayerId, player_id_2 : PlayerId) : GameState{
 
     let cards : Card[] = [];
     //get a random cards 
+    for (let nr_card = 0; nr_card < NUMBER_DIGIT_CARDS_IN_GAME; nr_card++){
+        cards.push(ALL_DIGIT_CARDS[Math.floor(Math.random() * ALL_DIGIT_CARDS.length)]);
+    }
+
+    for (let nr_card = 0; nr_card < NUMBER_OPERATION_CARDS_IN_GAME; nr_card++){
+        cards.push(ALL_OPERATION_CARDS[Math.floor(Math.random() * ALL_OPERATION_CARDS.length)]);
+    }
 
 
+    //Init a game
+    let game : GameState = {player1Id : player_id_1, player2Id : player_id_2, player1exp : [], player2exp : [], currentTurn : (Math.floor(Math.random() * 2) + 1 as 1 | 2), availableCards : cards};
 
-
-    let game : GameState = {player1Id : "2", player2Id : "2", player1exp : [], player2exp : [], currentTurn : 1, availableCards : cards};
-
-
-
-    return ;
+    return game;
 }
+
+
+
+function emit_game_state(game_id : number) : void{
+
+    const game : GameState = current_games.get(game_id); 
+
+    io.to(game.player1Id).emit("update-state", {game_id : game_id, game : game});
+    io.to(game.player2Id).emit("update-state", {game_id : game_id, game : game});
+}
+
+
 
 
 io.on('connection', (socket) => {
  
     //There is someone in queue
     if (players_queue.length > 0){
-        createGame(socket.id, players_queue.shift());
+        //Create new game and add it to the current games
+        let game : GameState = createGame(socket.id, players_queue.shift() as string);
+
+        const game_id = last_game_id + 1;
+        last_game_id = game_id;
+
+        current_games.set(game_id, game);
+
+        emit_game_state(game_id);
     }
     else{
         players_queue.push(socket.id);
