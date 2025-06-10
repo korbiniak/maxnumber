1d385cbebd225d886dc0349fd69be6ba6b99f39f
Mpdoge <154098343+Mpdoge@users.noreply.github.com>
2025-06-10 14:42:50 +0200

    Backend for the rooms (#6)
    Co-authored-by: Pr0grammerSimon <szymon@krzywda.eu>

--- client/src/pages/Rooms/Rooms.tsx
+++ client/src/pages/Rooms/Rooms.tsx
@@ -1,22 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import style from "./Rooms.module.css";
 import buttonStyle from "../../styles/button1.module.css";
-
+import socket from "../../socket";
+import type { Room } from "shared";
 
 export default function Rooms(){
 
-    const [publicRooms, setPublicRooms] = useState(["2dw1dw3g7", "qdw6wdwad9", "efedwwd22dw", "wddw2dw", "dor125jvmaksrs2dsdw"]);
+    const [rooms, setRooms] = useState< Room[]> ([]);
 
+     useEffect(() => {
+        socket.on("room-list", (data) => {
+            console.log("🔁 room-list z backendu:", data);
+            setRooms(data);
+        });
+
+        return () => {
+            socket.off("room-list");
+        };
+    }, []);
 
     return <div className={` ${style.container} ${buttonStyle}`}>
         <div className={`${style.left}`}>
-            <button>Create Public Room</button>
-            <button>Create Private Room</button>
+            <button>Create Room</button>
         </div>
         
         <div className={style.middle}>
-            <h1>Public Rooms</h1>
-            {publicRooms.map((room) => <button className={style.roomButton}>Join: {room}</button>)}
+            <h1>Available Rooms</h1>
+            {rooms.map((room) => <button className={style.roomButton}>Join: {room.id}</button>)}
         </div>
     </div>
 }
